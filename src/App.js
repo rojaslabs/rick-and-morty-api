f2fc5b42547e5a5699c67ffcac2f293dff0bfb6e
Jorge Rojas <exxscher@gmail.com>
2022-05-21 21:05:04 -0400

    Updated routes

--- src/App.js
+++ src/App.js
@@ -5,23 +5,24 @@ import Character from './views/Character'
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Router>
         <nav className='navbar navbar-expand-sm bg-dark navbar-dark'>
           <div className='container'>
-            <Link className='navbar-brand' to=''>Rick and Morty API</Link>
+            <Link className='navbar-brand' to='/'>Rick and Morty API</Link>
           </div>
         </nav>
 
         <div className='container p-5'>
           <Routes>
-            <Route index path='/' element={<Characters />} />
-            <Route path='character/:id' element={<Character/>} />
+            <Route path='character/:id' element={<Character />} />
+            <Route path='/' element={<Characters />}>
+            </Route>
           </Routes>
           <div className='text-center py-5'><a className='text-decoration-none' href='https://github.com/rojaslabs/rick-and-morty-api'>github.com/rojaslabs/rick-and-morty-api</a></div>
         </div>
       </Router>
-    </>
+    </div>
   )
 }
 
